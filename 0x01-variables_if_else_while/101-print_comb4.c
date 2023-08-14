a577af55d702ca855a526bf75313a4eba260d386
Teefah Pearl <lateefatalao787@gmail.com>
2023-08-14 15:55:14 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-int n, m, l
+int n, m, l;
 for (n = 48; n < 58; n++)
 {
 for (m = 49; m < 58; m++)
@@ -20,7 +20,7 @@ putchar(l);
 if (n != 55 || m != 56)
 {
 putchar(',');
-putchar(' ')
+putchar(' ');`
 }
 }
 }
