768cf311ad2d69134e7244a94a312ebbea75054e
Teefah Pearl <lateefatalao787@gmail.com>
2023-08-15 18:33:26 -0700

    xyz

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,10 +2,13 @@
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
- */
-int i, j, k;
+*/
+void print_times_table(int n)
 {
+int i, j, k;
 if (n >= 0 && n <= 15)
+{
+for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
@@ -40,5 +43,6 @@ _putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
+}
 }
 }
