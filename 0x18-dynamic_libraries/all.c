55ca753cb4ec20111869c4b67976970cf44d4736
Teefah_Pearl <lateefatalao787@gmail.com>
2024-01-19 14:31:38 -0800

    task 4

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a: first unt
+ * @b: second int
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sub - subtract two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtracted final result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+#include <stdio.h>
+
+/**
+ * div - divide two integers
+ * @a: first unt
+ * @b: second int
+ * Return: division result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - mod of two integers
+ * @a: first unt
+ * @b: second int
+ * Return: final answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
